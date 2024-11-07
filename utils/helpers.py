906b07e09bd8e473d40e3e338bea1f1cb3023f5d
Sherida101 <aaliyahsp@hotmail.com>
2024-11-07 12:49:57 +0000

    Renamed 'general' schema directory to 'base'; Converted error messages into a dictionary; Moved obsolete code to a separate file

--- utils/helpers.py
+++ utils/helpers.py
@@ -5,7 +5,21 @@ import os
 import pandas as pd
 import sys
 
-# Helpers: General
+# Helpers: Variables
+EXCLUDED_FILES = ['exclusions.json', 'sample_fields_core.json', 'sample_fields_extended.json']
+
+MAPPING = ['dwc', 'mixs', 'schemaorg', 'tol']
+
+SCHEMA_BASE_DIR_PATH = 'schemas/base'
+
+SCHEMA_FILE_PATHS = [f'{SCHEMA_BASE_DIR_PATH}/{filename}' for root, dirs, files in os.walk(SCHEMA_BASE_DIR_PATH) 
+                        for filename in files if filename.endswith('.json') and 
+                        filename not in EXCLUDED_FILES
+                    ]
+
+TERMSETS = ['core', 'extended']
+
+# Helpers: Mappings
 CHECKLIST_MAPPING = {
     'SCRNASEQ':{
         'accession': 'SCRNASEQ1',
@@ -30,63 +44,20 @@ CHECKLIST_MAPPING = {
     }
 }
 
-EXCLUDED_FILES = ['exclusions.json', 'sample_fields_core.json', 'sample_fields_extended.json']
+MESSAGES = {
+    'error_msg_invalid_file_path': f'Invalid .json schema file path. Please check the "{SCHEMA_BASE_DIR_PATH}" directory for available files',
+    'error_msg_invalid_standard': f"""Invalid standard. Please use {' or '.join([f'"{term}"' for term in MAPPING])} as standard.""",
+    'error_msg_invalid_termset': f"""Invalid termset. Please use {' or '.join([f'"{term}"' for term in TERMSETS])} as termset."""
+}
 
-SCHEMA_FILE_PATHS = [f'schemas/base/{filename}' for root, dirs, files in os.walk('schemas/base') 
-                        for filename in files if filename.endswith('.json') and 
-                        filename not in EXCLUDED_FILES
-                    ]
-mapping = ['dwc', 'mixs', 'schemaorg', 'tol']
-TERMSETS = ['core', 'extended']
-
+# Helpers: Functions
 def convertStringToTitleCase(text):
     # Convert given a string to title case/sentence case
     return text.title().replace('_', ' ')
 
-def create_field(line):
-    return {line['term_localName']: {'reference': line['iri'], 'required': False, 'type': 'string'}}
-
 def get_col_desc_eg(component, standard):
     field_validation = get_validation(component, standard)
     return {field: {'description': field_info.get('description', ''), 'example': field_info.get('example', '')} for field, field_info in field_validation.items()}
-
-# def get_dwc_fields(termset):
-#     '''
-#     This function reads a CSV file and a JSON file, filters the data from the CSV file based on certain conditions,
-#     and returns a list of dictionaries representing the filtered data.
-
-#     The CSV file 'schemas/dwc.csv' contains data with various fields. The JSON file 'schemas/exclusions.json' contains
-#     a list of labels that should be excluded from the final output.
-
-#     The function first reads the CSV file using pandas and loads the JSON file. It then filters the data from the CSV file
-#     to include only those rows where the 'status' field is either 'recommended' or 'required'. It also excludes any rows
-#     where the 'label' field is in the list of excluded labels from the JSON file.
-
-#     For each of the remaining rows, it creates a dictionary using the 'create_field' function and adds it to the output list.
-
-#     Returns:
-#         out (list): A list of dictionaries representing the filtered data from the CSV file.
-#     '''
-#     # Read the CSV file
-#     orig = pd.read_csv('schemas/dwc.csv')
-
-#     # Load the JSON file
-#     with open('schemas/exclusions.json') as excluded_json:
-#         excluded = json.loads(excluded_json.read())['excluded']
-
-#     # Filter the data from the CSV file
-#     filtered = orig[(orig.status == 'recommended')]
-
-#     # Create the output list
-
-#     if termset == 'extended':
-#         out = [create_field(line) for _, line in filtered.iterrows()]
-#     elif termset == 'core':
-#         out = [create_field(line) for _, line in filtered.iterrows() if
-#                line['term_localName'] in [item['name'] for item in excluded if item['set'] == 'core']]
-#     else:
-#         sys.exit("Invalid termset. Please use 'core' or 'extended' as termset.")
-#     return out
 
 def generate_json_file(data, output_file_path):
     '''
